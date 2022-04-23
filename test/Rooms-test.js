70362304b5e9fc886a65e04158f6ffc14d9dc803
Oakley Windiate <oakley.windiate@gmail.com>
2022-04-23 15:12:45 -0600

    Create test for findRoomNumber method

--- test/Rooms-test.js
+++ test/Rooms-test.js
@@ -41,25 +41,25 @@ describe('Rooms', () => {
         "id": "5fwrgu4i7k55hl6sz",
         "userID": 9,
         "date": "2022/04/22",
-        "roomNumber": 15
+        "roomNumber": 1
       },
       {
         "id": "5fwrgu4i7k55hl6t5",
         "userID": 43,
         "date": "2022/01/24",
-        "roomNumber": 24
+        "roomNumber": 2
       },
       {
         "id": "5fwrgu4i7k55hl6t6",
         "userID": 13,
         "date": "2022/01/10",
-        "roomNumber": 12
+        "roomNumber": 3
       },
       {
         "id": "5fwrgu4i7k55hl6t7",
         "userID": 20,
         "date": "2022/02/16",
-        "roomNumber": 7
+        "roomNumber": 4
       },
     ];
 
@@ -150,5 +150,34 @@ describe('Rooms', () => {
   it('should know the cost per night', () => {
     expect(room1.roomsData[0].costPerNight).to.equal(358.4);
     expect(room2.roomsData[1].costPerNight).to.equal(477.38);
+  });
+
+  it('should know the cost per night', () => {
+    expect(room1.roomsData[0].costPerNight).to.equal(358.4);
+    expect(room2.roomsData[1].costPerNight).to.equal(477.38);
+  });
+
+  it('should find a specific room number', () => {
+
+    expect(room1.findRoomNumber(1)).to.deep.equal([
+      {
+        "number": 1,
+        "roomType": "residential suite",
+        "bidet": true,
+        "bedSize": "queen",
+        "numBeds": 1,
+        "costPerNight": 358.4
+      }
+    ]);
+    expect(room2.findRoomNumber(3)).to.deep.equal([
+      {
+        "number": 3,
+        "roomType": "single room",
+        "bidet": false,
+        "bedSize": "king",
+        "numBeds": 1,
+        "costPerNight": 491.14
+      }
+    ]);
   });
 });
