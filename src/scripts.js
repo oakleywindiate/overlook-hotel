dc4703d2eb19d626a7c87ad4ff418a89e1e03399
Oakley Windiate <oakley.windiate@gmail.com>
2022-04-24 13:29:44 -0600

    Add import for flatpickr

--- src/scripts.js
+++ src/scripts.js
@@ -5,10 +5,19 @@
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
 
 import Customer from './classes/Customer';
-import Bookings from '.classes/Bookings';
-import Rooms from '.classes/Rooms';
+// import Bookings from '.classes/Bookings';
+// import Rooms from '.classes/Rooms';
 import apiCalls from './apiCalls';
-import {fetchData}
+import flatpickr from "flatpickr";
+// import {fetchData}
+import 'flatpickr/dist/flatpickr.css';
+require("flatpickr/dist/themes/dark.css");
+
+flatpickr("input[type=date]", {
+  altInput: true,
+  altFormat: "F j, Y",
+  dateFormat: "Y-m-d",
+})
