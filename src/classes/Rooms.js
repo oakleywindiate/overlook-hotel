422f1cebf20facdc771ba00be17664da49a02728
Oakley Windiate <oakley.windiate@gmail.com>
2022-04-23 23:44:53 -0600

    Create methods for: findNumber and findRoomCostPerNight

--- src/classes/Rooms.js
+++ src/classes/Rooms.js
@@ -5,6 +5,10 @@ class Rooms {
   findNumber(id) {
     return Number(Object.values(this.roomsData).filter(rooms => rooms.number === id).map(obj => obj.number))
   }
+
+  findRoomCostPerNight(id) {
+    return Number(Object.values(this.roomsData).filter(rooms => rooms.number === id).map(obj => obj.costPerNight))
+  }
 };
 
 export default Rooms;
