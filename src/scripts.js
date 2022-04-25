9d6d45d4b881f9d4ef641ff401bd83db35dc10ec
Oakley Windiate <oakley.windiate@gmail.com>
2022-04-24 20:45:21 -0600

    Create functions for displaying all bookings and total amount spent

--- src/scripts.js
+++ src/scripts.js
@@ -1,18 +1,11 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
+// ---------------- IMPORTS ---------------- //
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-// import './images/turing-logo.png'
-
 import Customer from './classes/Customer';
-// import Bookings from '.classes/Bookings';
-// import Rooms from '.classes/Rooms';
+import Bookings from './classes/Bookings';
+import Rooms from './classes/Rooms';
 import apiCalls from './apiCalls';
 import flatpickr from "flatpickr";
-// import {fetchData}
+import {fetchData} from './apiCalls.js';
 import 'flatpickr/dist/flatpickr.css';
 require("flatpickr/dist/themes/dark.css");
 
@@ -21,3 +14,66 @@ flatpickr("input[type=date]", {
   altFormat: "F j, Y",
   dateFormat: "Y-m-d",
 })
+
+// ---------------- QUERY SELECTORS ---------------- //
+
+let injectBookings = document.querySelector('.display-bookings');
+let injectTotalSpent = document.querySelector('.display-customer-amount-spent');
+
+// ---------------- GLOBAL VARIABLES ---------------- //
+
+let customerRepo;
+let roomsRepo;
+let bookingsRepo;
+let customer;
+
+// ---------------- FUNCTIONS ---------------- //
+
+const retrieveRandomCustomer = (value) => {
+  const customer = value[Math.floor(Math.random() * value.length)];
+  return customer;
+};
+
+const fetchApiData = () => {
+  Promise.all([
+    fetchData('customers'),
+    fetchData('rooms'),
+    fetchData('bookings')
+  ]).then(data => {
+    createInstances(data)
+    displayCustomerInfomation(customer.id, bookingsRepo, roomsRepo)
+    displayTotalAmountSpent(customer.id, roomsRepo, bookingsRepo)
+  });
+};
+
+const createInstances = (data) => {
+  customerRepo = new Customer(data[0].customers);
+  roomsRepo = new Rooms(data[1].rooms);
+  bookingsRepo = new Bookings(data[2].bookings);
+  customer = retrieveRandomCustomer(customerRepo.customerData);
+  console.log(customer)
+};
+
+const displayCustomerInfomation = (id, bookingRepo, roomsRepo) => {
+  return bookingRepo.findUserObject(id).forEach(booking => {
+    roomsRepo.findRoomObject(booking.roomNumber).map(room => {
+      injectBookings.innerHTML +=
+      `<li class="bed-size">bed size: ${room.bedSize}</li>
+      <li class="bed-size">bidet: ${room.bidet}</li>
+      <li class="bed-size">cost per night: $${room.costPerNight}</li>
+      <li class="bed-size">number of beds: ${room.numBeds}</li>
+      <li class="bed-size">room type: ${room.roomType}</li>
+      <li class="bed-size">booking date: ${booking.date}</li>
+      <li class="bed-size">room number: ${booking.roomNumber}</li>`
+    })
+  })
+};
+
+const displayTotalAmountSpent = (id, roomsRepo, bookingsRepo) => {
+  injectTotalSpent.innerHTML += `<li class="amount-spent">Amount Spent: $${customerRepo.totalAmountSpent(id, roomsRepo, bookingsRepo)}`
+};
+
+
+// ---------------- FUNCTIONS ---------------- //
+
+window.addEventListener('load', fetchApiData);
