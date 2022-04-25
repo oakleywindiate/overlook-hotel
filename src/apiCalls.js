0c6612eb09e1b081c036b84162a5d5a14bfc47d6
Oakley Windiate <oakley.windiate@gmail.com>
2022-04-25 12:39:17 -0600

    Create rudimentary post function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,3 +8,47 @@ const fetchData = (address) => {
 };
 
 export {fetchData}
+
+// const addIngredients = (newIngredient) => {
+//   fetch("http://localhost:3001/api/v1/users", {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(newIngredient)
+//   })
+//   .then(response => {
+//     if (!response.ok) {
+//       throw Error()
+//     } else {
+//       return response.json(response.statusText)
+//     }
+//   })
+//   .then(response => refreshPantry(newIngredient.userID))
+//   .catch(error => {
+//     showError('There was an issue adding this ingredient. Try again!')
+//   });
+// };
+//
+// const removeIngredients = (newIngredient) => {
+//   fetch("http://localhost:3001/api/v1/users", {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(newIngredient)
+//   })
+//   .then(response => {
+//     if (!response.ok) {
+//       throw Error(response.statusText)
+//     } else {
+//       return response.json()
+//     }
+//   })
+//   .then(response => refreshPantry(newIngredient.userID))
+//   .catch(error => {
+//     showError('There was an issue adding this ingredient. Try again!')
+//   });
+// };
+//
+// const showError = (message) => {
+//   errorMessage.innerText = message;
+// };
+//
+// export {getFetch, addIngredients, removeIngredients, errorMessage};
