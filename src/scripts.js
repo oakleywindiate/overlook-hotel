ef0c531af702a8bba08959172c36b6228ffbebd7
Oakley Windiate <oakley.windiate@gmail.com>
2022-04-25 22:29:58 -0600

    Create function for displaying chart

--- src/scripts.js
+++ src/scripts.js
@@ -9,6 +9,14 @@ import {fetchData, addBooking} from './apiCalls.js';
 import 'flatpickr/dist/flatpickr.css';
 require("flatpickr/dist/themes/dark.css");
 
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+
+var ctx = document.getElementById('myChart');
+var ctx = document.getElementById('myChart').getContext('2d');
+
+import {amountSpent} from './charts.js';
+
 flatpickr("input[type=date]", {
   altInput: true,
   altFormat: "F j, Y",
@@ -28,6 +36,9 @@ let modal = document.querySelector('.modal');
 let close = document.querySelector('.close');
 let modalContent = document.querySelector('.modal-content');
 let bookRoom = document.querySelector('.book-room');
+let successModal = document.querySelector('.success-modal');
+let successClose = document.querySelector('.success-close');
+let successModalContent = document.querySelector('.success-modal-content');
 
 // ---------------- GLOBAL VARIABLES ---------------- //
 
@@ -58,6 +69,12 @@ const fetchApiData = () => {
   });
 };
 
+const updateApi = () => {
+  fetchData(`customers/${customer.id}`)
+  fetchData('bookings')
+  clearPage()
+}
+
 const createInstances = (data) => {
   customerRepo = new Customer(data[0].customers);
   roomsRepo = new Rooms(data[1].rooms);
@@ -84,25 +101,35 @@ const displayCustomerInfomation = (id, bookingRepo, roomsRepo) => {
 };
 
 const displayTotalAmountSpent = (id, roomsRepo, bookingsRepo) => {
-  injectTotalSpent.innerHTML += `
+  // const total = 9000
+  const amount = injectTotalSpent.innerHTML += `
   <li class="amount-spent">Amount Spent: $${customerRepo.totalAmountSpent(id, roomsRepo, bookingsRepo)}</li>`
+  amountSpent(ctx, customerRepo.totalAmountSpent(id, roomsRepo, bookingsRepo), 3000)
 };
 
 const displaySelectedRoom = (id) => {
   injectNoEvents(modalContent, (roomsRepo.findRoomObject(id)[0]))
-}
-
+};
 
 const findAvailableRooms = (date) => {
+  availableRooms = [];
   return roomsRepo.roomsData.forEach(room => {
-    if (!bookingsRepo.findDate(date).includes(room.number)) {
+    if (bookingsRepo.findDate(date).length === 0) {
+        roomsRepo.findRoomObject(room.number).forEach(obj => {
+          availableRooms.push(obj)
+          injectSearch(injectDateSearch, obj)
+        })
+    } else if (!bookingsRepo.findDate(date).includes(room.number)) {
       roomsRepo.findRoomObject(room.number).forEach(obj => {
         availableRooms.push(obj)
         injectSearch(injectDateSearch, obj)
       })
+    } else if (bookingsRepo.findDate(date).length > 0 && (!bookingsRepo.findDate(date).includes(room.number))) {
+        runNoResultsModal()
     }
   })
 };
+
 
 const searchRoomTypes = () => {
   return availableRooms.filter(room => {
@@ -110,6 +137,8 @@ const searchRoomTypes = () => {
         injectSearch(searchRoomType, room)
     } else if (searchRoomValue.value === 'all rooms') {
         injectSearch(injectDateSearch, room)
+      } else {
+        runNoResultsModal()
       }
     })
   };
@@ -137,6 +166,21 @@ const injectNoEvents = (element, obj) => {
   </section>`
 }
 
+const displaySuccessfulBooking = () => {
+  modal.style.display = 'none';
+  clearHtml(modalContent)
+  clearHtml(successModalContent)
+  successModal.style.display = 'block';
+  successModalContent.innerHTML += `
+  <h2 class="success-message">Congratulations, ${customerRepo.findCustomerName(customer.id)[0]}!
+  <p>You have successfully booked this room.`
+};
+
+const runNoResultsModal = () => {
+  modal.style.display = 'block';
+  modalContent.innerHTML += `
+  <p>Oh no! Looks like there are no available rooms for this date. Please try again!</p>`
+}
 
 
 // ---------------- DYNAMIC FUNCTIONS ---------------- //
@@ -178,6 +222,7 @@ const sendApiData = (date, roomNumber) => {
       roomNumber: parseInt(roomNumber)
     };
   addBooking(newBooking)
+  displaySuccessfulBooking()
 };
 
 calendarForm.addEventListener('submit', (e) => {
@@ -199,7 +244,11 @@ window.addEventListener('load', fetchApiData);
 // window.addEventListener('click', fetchApiData);
 
 injectDateSearch.addEventListener('click', (e) => {
-  // let targetId = Number(e.target.parentElement.getAttribute('id'))
+  displaySelectedRoom(Number(e.target.parentElement.getAttribute('id')))
+  modal.style.display = 'block';
+});
+
+searchRoomType.addEventListener('click', (e) => {
   displaySelectedRoom(Number(e.target.parentElement.getAttribute('id')))
   modal.style.display = 'block';
 });
@@ -212,4 +261,12 @@ close.addEventListener('click', (e) => {
 modalContent.addEventListener('click', (e) => {
   e.preventDefault();
   findPostInformaton(Number(e.target.getAttribute('id')))
+  displaySuccessfulBooking()
+});
+
+successClose.addEventListener('click', (e) => {
+  successModal.style.display = 'none';
+  clearHtml(successModalContent)
+  updateApi()
+
 });
